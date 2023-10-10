8fda6bf433c42782145b803637afd2069c81b7de
Piyush Yadav <piyushyadav0191@gmail.com>
2023-10-10 09:30:34 +0530

    progress bar added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,41 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import { ThemeProvider } from '@/providers/theme';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import type { FC } from '@/types';
 import { darkTheme, globalStyles } from '~/stitches';
 import '@/styles/globals.scss';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+NProgress.configure({
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   globalStyles();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const start = () => {
+      NProgress.start();
+    };
+    const end = () => {
+      NProgress.done();
+    };
+    router.events.on('routeChangeStart', start);
+    router.events.on('routeChangeComplete', end);
+    router.events.on('routeChangeError', end);
+    return () => {
+      router.events.off('routeChangeStart', start);
+      router.events.off('routeChangeComplete', end);
+      router.events.off('routeChangeError', end);
+    };
+  }, [router.events]);
   return (
     <>
       <Head>
